39dba40c5968fc8b689cc5027b522cb689c3c91b
Marco Filannino <filanninobusiness@gmail.com>
2025-02-10 18:13:04 +0100

    Aggiunto esercizio http

---
+++ server.mjs
@@ -0,0 +1,13 @@
+import { createServer } from "node:http";
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" }); // Imposta il codice di stato e l'intestazione
+  res.end("<h1>Ciao, questo è il mio server HTTP in Node.js!</h1>"); // Corpo della risposta HTML
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server in esecuzione su http://localhost:${PORT}`);
+});
